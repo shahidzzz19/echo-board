bd8078f19b7242b40b363eda7c79114be6418b15
Mohammed Shahid <shahidsait19@gmail.com>
2025-09-06 00:04:01 +0530

    Resolve commit

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,12 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+export const safeImage = (url: string | null | undefined) =>
+  url && url.startsWith("http") ? url : null;
+
+export const generateId = (prefix: string, unique: string | number) =>
+  `${prefix}-${unique}-${Date.now()}`;
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
