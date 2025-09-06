45c648ef452074fd8f759ed1aaf39c2e760e33d2
Mohammed Shahid <shahidsait19@gmail.com>
2025-09-06 16:01:46 +0530

    Fix debounce hook typings + add ESLint Next plugin

--- lib/hooks/useDebounce.ts
+++ lib/hooks/useDebounce.ts
@@ -1,20 +1,24 @@
-"use client"
+"use client";
 
-import { useCallback, useRef } from "react"
+import { useCallback, useRef } from "react";
 
-export function useDebounce<T extends (...args: any[]) => void>(callback: T, delay: number): T {
-  const timeoutRef = useRef<NodeJS.Timeout>()
+export function useDebounce<T extends (...args: any[]) => void>(
+  callback: T,
+  delay: number
+): T {
+  // Always initialize useRef with null and handle cleanup
+  const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
 
   return useCallback(
     ((...args: Parameters<T>) => {
       if (timeoutRef.current) {
-        clearTimeout(timeoutRef.current)
+        clearTimeout(timeoutRef.current);
       }
 
       timeoutRef.current = setTimeout(() => {
-        callback(...args)
-      }, delay)
+        callback(...args);
+      }, delay);
     }) as T,
-    [callback, delay],
-  )
+    [callback, delay]
+  );
 }
